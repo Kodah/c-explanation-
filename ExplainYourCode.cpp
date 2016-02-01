13a6c9c76562cc89cdcd9d5a0eabf8938a857f4d
Tom Sugarev <Kodah@users.noreply.github.com>
2016-02-01 23:43:22 +0000

    Update more readable

--- ExplainYourCode.cpp
+++ ExplainYourCode.cpp
@@ -1,39 +1,24 @@
-// ExplainYourCode.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <typeinfo>
-
 // This is a functor that overrides the function call () operator 
-struct F
-{
-	void operator() (int x)
-	{
+struct F {
+	void operator() (int x)	{
 		// do something
 		std::cout << x;
 	}
 };
-
-int main()
-{
+int main() {
 	std::vector<int> v;
-	
 	// more code, not shown here.
-	for (auto i = 0; i < 10; i++)
-	{
-		v.push_back(i);
-	}
-	 /*
-	 The third parameter of the for_each algorithem can be any callable object. 
-	 The callable object is applied to every dereferenced iterator between v.begin() and v.end()
-	 You could have a inline lambda if you wanted [](int x) -> void {std::cout << x; }
+
+	std::for_each(v.begin(), v.end(), F());
+	/*
+	The third parameter of the for_each algorithm can be any callable object. 
+	Every iterator in the vector is dereferenced and passed to the callable object as a parameter 
+	You could have a inline lambda if you wanted [](int x) -> void {std::cout << x; }
+	It must match the signature required by for_each, one parameter.
+	otherwise you will get an error like 'no matching overloaded function found'.
 	 */
 
 	// The prototype 
-	/*
 	template<class InputIterator, class Function>
 	Function for_each(InputIterator first, InputIterator last, Function f)
 	{
@@ -41,18 +26,12 @@ int main()
 		f(*first);
 		return f;
 	}
-
-	Templated generates code at compile time. 
-	F() makes instance of the functor. It must match the signature required by for_each, one parameter.
-	otherwise you will get like 'no matching overloaded function found'
-
-	functor destroyed when for_each finishes
-
+	/*
+	For_each is a function template, the compiler generates code at compile time.
+	F() makes instance of the functor which gets destroyed when for_each finishes.
+	You could make an instance of F f; and use that without the brackets if you wanted
+	to access the struct again for some reason.
+	std::for_each(v.begin(), v.end(), f;
 	*/
-	std::for_each(v.begin(), v.end(), F());
-
-	std::cin.get();
-
-    return 0;
 }
 
