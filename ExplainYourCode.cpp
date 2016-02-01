baf6c27e33aeb8ecc85f76a9a83caff9082f9384
Kodah <tom_1440@hotmail.co.uk>
2016-02-01 23:28:14 +0000

    done

--- ExplainYourCode.cpp
+++ ExplainYourCode.cpp
@@ -5,15 +5,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <typeinfo>
 
-
-struct f
+// This is a functor that overrides the function call () operator 
+struct F
 {
-	void operator() (int n)
+	void operator() (int x)
 	{
-		//do something
-		std::cout << n;
-		
+		// do something
+		std::cout << x;
 	}
 };
 
@@ -22,16 +22,34 @@ int main()
 	std::vector<int> v;
 	
 	// more code, not shown here.
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-	v.push_back(4);
-	v.push_back(5);
-	v.push_back(6);
+	for (auto i = 0; i < 10; i++)
+	{
+		v.push_back(i);
+	}
+	 /*
+	 The third parameter of the for_each algorithem can be any callable object. 
+	 The callable object is applied to every dereferenced iterator between v.begin() and v.end()
+	 You could have a inline lambda if you wanted [](int x) -> void {std::cout << x; }
+	 */
 
-	f funct;
+	// The prototype 
+	/*
+	template<class InputIterator, class Function>
+	Function for_each(InputIterator first, InputIterator last, Function f)
+	{
+		for (; first != last; ++first) {
+		f(*first);
+		return f;
+	}
 
-	std::for_each(v.begin(), v.end(), funct);
+	Templated generates code at compile time. 
+	F() makes instance of the functor. It must match the signature required by for_each, one parameter.
+	otherwise you will get like 'no matching overloaded function found'
+
+	functor destroyed when for_each finishes
+
+	*/
+	std::for_each(v.begin(), v.end(), F());
 
 	std::cin.get();
 
