65324adc9723dc984d8b448ee19064fdee9fa953
Tom Sugarev <Kodah@users.noreply.github.com>
2016-02-02 00:18:39 +0000

    cf

--- ExplainYourCode.cpp
+++ ExplainYourCode.cpp
@@ -18,7 +18,7 @@ int main() {
 	otherwise you will get an error like 'no matching overloaded function found'.
 	 */
 
-	// The prototype 
+	// The prototype possible implementation
 	template<class InputIterator, class Function>
 	Function for_each(InputIterator first, InputIterator last, Function f)
 	{
